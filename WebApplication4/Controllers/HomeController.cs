98565efea5e14a30d2638150aaf3e2a713a9ba8c
Loefflerm <Loefflerm@H10DCPE72536>
2017-04-25 08:50:00 -0400

    Add project files.

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
+
+        public ActionResult Rendercta()
+
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_cta.cshtml");
+        }
+        public ActionResult RenderFeatured()
+
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Featured.cshtml");
+        }
+        public ActionResult RenderContact()
+
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+    }
+}
---
+++ WebApplication4/Controllers/SiteLayoutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+
+
+namespace WebApplication4
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
+        public ActionResult RenderHeader()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Header.cshtml");
+        }
+        public ActionResult RenderIntro()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Intro.cshtml");
+        }
+        public ActionResult RenderFooter()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Footer.cshtml");
+        }
+    }
+}
